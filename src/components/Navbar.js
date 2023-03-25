2bece5d9b7723509b3f2326717d38274c3207f5a
sebastian.andersson.kallman <sebastian.andersson.kallman@decerno.se>
2023-03-25 13:40:44 +0100

    fixed logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "gatsby";
 import github from "../img/github-icon.svg";
-import logo from "../../static/img/logo-text-1.png";
+import logo from "../img/logo.svg";
 
 const Navbar = () => {
   const [isActive, setIsActive] = useState(false);
