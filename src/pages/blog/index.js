728ee6eb380f06b9a94f17c3990c9e33a166da1c
sebastian.andersson.kallman <sebastian.andersson.kallman@decerno.se>
2023-03-27 16:58:19 +0200

    oops

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -10,7 +10,7 @@ export default class BlogIndexPage extends React.Component {
         <div
           className="full-width-image-container margin-top-0"
           style={{
-              backgroundImage: `url('/img/dark_logos_tech.jpeg')`,
+              backgroundImage: `url('/img/abstract_background_top_color.jpeg')`,
           }}
         >
           <h1
